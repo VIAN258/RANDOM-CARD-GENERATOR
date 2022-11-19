450f4b3255ac1f3e2d729d4dc3e0bcba52b8f04c
VIAN258 <angelmorales891@gmail.com>
2022-11-19 13:01:21 +0000

    testcambio de color

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import "./assets/img/4geeks.ico";
 
 const numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const simbolos = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+const colores = ["black", "red", "black"];
 
 function numeroAleatorio() {
   let aleatorio = Math.floor(Math.random() * 12);
@@ -31,6 +32,11 @@ function intercambiar2() {
 function intercambiar3() {
   let numero = numeroAleatorio();
   document.getElementById("medio").innerHTML = numero;
+}
+
+function colorAleatorio() {
+  let aleatorio = Math.floor(Math.random() * colores.lenght);
+  body.style.color = colores[aleatorio];
 }
 
 setInterval(intercambiar1(), 1000);
