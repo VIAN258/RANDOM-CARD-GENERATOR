13fe871f952e36c97730c28b0d927b378c976f4f
VIAN258 <angelmorales891@gmail.com>
2022-11-19 00:37:59 +0000

    primera

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const simbolos = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+
+//document.getElementById("alto").innerHTML = "2";
+//document.getElementById("medio").innerHTML = "3";
+//document.getElementById("bajo").innerHTML = "7";
+
+function numeroAleatorio() {
+  let aleatorio = Math.floor(Math.random() * 12);
+  return numero[aleatorio];
+}
+
+function simbolosAleatorio() {
+  let aleatorio = Math.floor(Math.random() * 3);
+  return simbolos[aleatorio];
+}
+
+function intercambiar() {}
+
+setInterval(function() {
+  console.log("angel");
+}, 1000);
+
+//console.log(numeroAleatorio());
+//console.log(simbolosAleatorio());
